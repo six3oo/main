99b035cf79392ac5d9c1903266c4083f27e1877b
antonchua <emanasaur@gmail.com>
2017-10-02 16:23:12 +0800

    Add implementation of deleteTag(Tag) in ModelManager. Loops through each person, and removes the <Tag> from each person. Update Model API to suit.

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -2,10 +2,12 @@ package seedu.address.model;
 
 import java.util.function.Predicate;
 
+import com.sun.org.apache.bcel.internal.generic.DUP;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 
 /**
  * The API of the Model component.
@@ -36,6 +38,7 @@ public interface Model {
     void updatePerson(ReadOnlyPerson target, ReadOnlyPerson editedPerson)
             throws DuplicatePersonException, PersonNotFoundException;
 
+    void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException;
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<ReadOnlyPerson> getFilteredPersonList();
 
