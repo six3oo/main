48a69a91f05697b630b3549699112e794d717574
minghui94 <e0030856@u.nus.edu>
2017-10-07 21:27:57 +0800

    Update PersonCard.java
    Reordered imports

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Random;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -7,8 +9,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.ReadOnlyPerson;
-import java.util.HashMap;
-import java.util.Random;
 
 /**
  * An UI component that displays information of a {@code Person}.
