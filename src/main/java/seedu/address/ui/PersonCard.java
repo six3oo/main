775ddbabd7f84a0c74000a13c452e81430a4e652
minghui94 <e0030856@u.nus.edu>
2017-10-07 21:57:54 +0800

    Update PersonCard.java

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -17,7 +17,7 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
-    private static String[9] Colors = {"black", "blue", "brown", "green", "grey", "orange", "pink", "red", "yellow"};
+    private static String[] Colors = {"black", "blue", "brown", "green", "grey", "orange", "pink", "red", "yellow"};
     private static HashMap<String, String> tagColors = new HashMap<String, String>();
     private static Random random = new Random();
 
