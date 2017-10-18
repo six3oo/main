399920dc75a7afc578d37c715960d080232d73ec
minghui94 <e0030856@u.nus.edu>
2017-10-19 03:27:46 +0800

    Update AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -23,10 +22,6 @@ import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.SendCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.logic.commands.*;
-
-
-
 
 /**
  * Parses user input.
