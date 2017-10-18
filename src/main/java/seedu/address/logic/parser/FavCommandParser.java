bb3963d38be14064f6252ae54ca4a243479bd343
LAKESKY\Antz <emanasaur@gmail.com>
2017-10-18 20:56:55 +0800

    Update FavCommandParser to parse new input format

--- src/main/java/seedu/address/logic/parser/FavCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FavCommandParser.java
@@ -7,10 +7,14 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.FavCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import java.util.ArrayList;
+
 /**
  * Parses input arguments and creates a new FavCommand object
  */
 public class FavCommandParser implements Parser<FavCommand> {
+    public boolean status;
+    public String[] argArray;
 
     /**
      * Parses the given {@code String} of arguments in the context of the FavCommand
@@ -19,8 +23,16 @@ public class FavCommandParser implements Parser<FavCommand> {
      */
     public FavCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new FavCommand(index);
+            argArray = args.split("\\s+");
+            if (argArray[2] == "true"){
+                status = true;
+            }
+
+            else if (argArray[2] == "false"){
+                status = false;
+            }
+            Index index = ParserUtil.parseIndex(argArray[1]);
+            return new FavCommand(index, status);
         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FavCommand.MESSAGE_USAGE));
