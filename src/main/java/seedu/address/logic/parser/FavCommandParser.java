6328dacfb0daa3be8bdf0351731afe7463601119
minghui94 <e0030856@u.nus.edu>
2017-10-19 03:57:47 +0800

    Update FavCommandParser.java

--- src/main/java/seedu/address/logic/parser/FavCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FavCommandParser.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.FavCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
