2e5c37a995571bec1e83774c34f623357fe65a05
minghui94 <e0030856@u.nus.edu>
2017-10-22 15:51:53 +0800

    Create FindTagCommand.java
    A new command to allow user to find a contact via tag

---
+++ src/main/java/seedu/address/logic/commands/FindTagCommand.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.tag.TagContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all persons in address book whose tag contains any of the argument keywords.
+ * Keyword matching is case sensitive.
+ */
+public class FindTagCommand extends Command {
+
+    public static final String COMMAND_WORD = "findtag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + "Find all persons whose tags contain any of "
+            + "the specific keyword (case-sensitive) and display them as a list with index numbers.\n"
+            + "Parameters: [MORE KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " friends colleague family";
+
+    private final TagContainsKeywordsPredicate predicate;
+
+    public FindTagCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof FindTagCommand
+                && this.predicate.equals(((FindTagCommand) other).predicate));
+    }
+}
