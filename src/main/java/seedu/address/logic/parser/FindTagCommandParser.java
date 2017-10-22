083965b65dc49bee9c0fb95015756a912adab35c
minghui94 <e0030856@u.nus.edu>
2017-10-22 15:42:42 +0800

    Create FindTagCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/FindTagCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindTagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.TagContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindTagCommand object
+ */
+public class FindTagCommandParser implements Parser<FindTagCommand> {
+
+    /**
+     *  parses the given {@code String} of arguments in the context of the FindTagCommand
+     *  and returns an FindTagCommand object for execution.
+     *  @throws ParseException if the user input does not conform the expected format
+     */
+
+    @Override
+    public FindTagCommand parse(String userInput) throws ParseException {
+        String trimmedInput = userInput.trim();
+        if(trimmedInput.isEmpty()){
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+        }
+
+        String[] tagKeywords = trimmedInput.split("\\s+");
+        return new FindTagCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
+    }
+}
