f2658154b0ad395093f9695fae6e5edda30ab996
LAKESKY\Antz <emanasaur@gmail.com>
2017-10-25 22:50:43 +0800

    Add getFavourite() to Person

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -108,6 +108,11 @@ public class Person implements ReadOnlyPerson {
         this.favourite = fav;
     }
 
+    @Override
+    public boolean getFavourite() {
+        return this.favourite;
+    }
+
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
      * if modification is attempted.
