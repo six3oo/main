43188236c84199a7ae92ec31212ccfca51e87b07
LAKESKY\Antz <emanasaur@gmail.com>
2017-11-02 03:26:14 +0800

    Edit FavouritePredicate

--- src/main/java/seedu/address/model/person/FavouritePredicate.java
+++ src/main/java/seedu/address/model/person/FavouritePredicate.java
@@ -1,4 +1,19 @@
 package seedu.address.model.person;
 
-public class FavouritePredicate {
+import java.util.function.Predicate;
+
+public class FavouritePredicate implements Predicate<ReadOnlyPerson>{
+
+    @Override
+    public boolean test(ReadOnlyPerson readOnlyPerson) {
+        boolean result = false;
+        System.out.print("PREDICATE RUN: " + readOnlyPerson.getFavourite()+"\n");
+        if (readOnlyPerson.getFavourite().toString().equals("true")) {
+            result = true;
+        }
+        else if (readOnlyPerson.getFavourite().toString().equals("false")) {
+            result = false;
+        }
+        return result;
+    }
 }
