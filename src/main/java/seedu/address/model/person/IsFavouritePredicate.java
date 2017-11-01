c3ea9fad6c1f346ee1867b8c7abeb378b0422249
LAKESKY\Antz <emanasaur@gmail.com>
2017-11-01 22:11:35 +0800

    Simplify IsFavouritePredicate

--- src/main/java/seedu/address/model/person/IsFavouritePredicate.java
+++ src/main/java/seedu/address/model/person/IsFavouritePredicate.java
@@ -6,14 +6,9 @@ import java.util.function.Predicate;
  * Tests if a {@code ReadOnlyPerson}'s {@code Favourite} variable is true.
  */
 public class IsFavouritePredicate implements Predicate<ReadOnlyPerson> {
-    private final boolean fave;
-
-    public IsFavouritePredicate(boolean fave) {
-        this.fave = fave;
-    }
 
     @Override
     public boolean test(ReadOnlyPerson person) {
-        return fave;
+        return person.getFavourite();
     }
 }
