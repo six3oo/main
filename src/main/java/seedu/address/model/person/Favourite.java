90e064ed7fd1d96e9c182bca38d9f5855f682e4a
LAKESKY\Antz <emanasaur@gmail.com>
2017-11-02 03:25:51 +0800

    Implement Favourite class

--- src/main/java/seedu/address/model/person/Favourite.java
+++ src/main/java/seedu/address/model/person/Favourite.java
@@ -1,4 +1,19 @@
 package seedu.address.model.person;
 
 public class Favourite {
+
+    private boolean favourite;
+    public Favourite(String faveState) {
+        if (faveState.equals("true")){
+            this.favourite = true;
+        }
+        else {
+            this.favourite = false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(favourite);
+    }
 }
