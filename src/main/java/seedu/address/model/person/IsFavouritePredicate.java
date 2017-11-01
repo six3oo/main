28a1a6c5e364149522975566e9d462c268176077
LAKESKY\Antz <emanasaur@gmail.com>
2017-11-01 22:08:55 +0800

    Edit IsFavouritePredicate

--- src/main/java/seedu/address/model/person/IsFavouritePredicate.java
+++ src/main/java/seedu/address/model/person/IsFavouritePredicate.java
@@ -8,19 +8,12 @@ import java.util.function.Predicate;
 public class IsFavouritePredicate implements Predicate<ReadOnlyPerson> {
     private final boolean fave;
 
-    public isFavouritePredicate(boolean fave) {
+    public IsFavouritePredicate(boolean fave) {
         this.fave = fave;
     }
 
     @Override
     public boolean test(ReadOnlyPerson person) {
         return fave;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof IsFavouritePredicate // instanceof handles nulls
-                && this.fave.equals(((IsFavouritePredicate) other).fave)); // state check
     }
 }
