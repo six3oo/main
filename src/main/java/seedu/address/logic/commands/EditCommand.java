b1c07b6217a1129625a9b68ee307da25820ded8f
LAKESKY\Antz <emanasaur@gmail.com>
2017-11-02 04:54:45 +0800

    Yet MORE Travis CI checkstyle fixes

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -26,7 +26,6 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
 
-
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
