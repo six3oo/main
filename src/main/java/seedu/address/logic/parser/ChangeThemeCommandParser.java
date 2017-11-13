71883b86211f168bb2336af6939b310278b44e2f
Cheng Wei Hong <moomeowroar@users.noreply.github.com>
2017-11-13 11:02:57 +0800

    ChangeTheme Unit test

--- src/main/java/seedu/address/logic/parser/ChangeThemeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ChangeThemeCommandParser.java
@@ -35,10 +35,10 @@ public class ChangeThemeCommandParser implements Parser<ChangeThemeCommand> {
                 }
             }
             throw new ParseException(
-                    String.format(ChangeThemeCommand.MESSAGE_NO_THEME, ChangeThemeCommand.MESSAGE_USAGE));
+                    ChangeThemeCommand.MESSAGE_NO_THEME + "\n" +ChangeThemeCommand.MESSAGE_USAGE);
         } catch (IllegalValueException ive) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangeThemeCommand.MESSAGE_USAGE));
+                    ChangeThemeCommand.MESSAGE_NO_THEME + "\n" +ChangeThemeCommand.MESSAGE_USAGE);
         }
     }
 
